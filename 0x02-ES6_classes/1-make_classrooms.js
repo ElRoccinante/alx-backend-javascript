16eb9c9ccdbac0ff6c3548d6a0b98df39028430f
ElRoccinante <alaealchi@gmail.com>
2024-03-07 15:55:24 +0000

    h

--- 0x02-ES6_classes/1-make_classrooms.js
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -1,9 +1,9 @@
 import ClassRoom from './0-classroom';
 export default function initializeRooms() {
-        let taha = [
+        let initializeRooms = [
         new ClassRoom(19),
         new ClassRoom(20),
 	new ClassRoom(34),
 	];
-	return taha
+	return initializeRooms
 }
