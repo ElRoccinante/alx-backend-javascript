a1a45f4030c8fad7f2a7684593cdc5fc7a47a749
ElRoccinante <alaealchi@gmail.com>
2024-03-09 17:40:07 +0000

    h

--- 0x00-ES6_basic/10-loops.js
+++ 0x00-ES6_basic/10-loops.js
@@ -1,7 +1,8 @@
 export default function appendToEachArrayValue(array, appendString) {
-  const array0 = [];
-  for (const idx of array) {
-    array0.push(appendString + idx);
-  }
-  return array0;
+  const p = array;
+  array.forEach((value, idx) => {
+    p[idx] = appendString + value;
+  });
+
+  return p;
 }
