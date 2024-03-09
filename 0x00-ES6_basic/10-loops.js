599c51c598bc51c23ed061be0c18130e7dc47bc2
ElRoccinante <alaealchi@gmail.com>
2024-03-09 17:54:10 +0000

    tying other technique task 10 in basic

--- 0x00-ES6_basic/10-loops.js
+++ 0x00-ES6_basic/10-loops.js
@@ -1,8 +1,7 @@
 export default function appendToEachArrayValue(array, appendString) {
-  const p = array;
-  array.forEach((value, idx) => {
-    p[idx] = appendString + value;
-  });
-
-  return p;
+  const array0 = [];
+  for (const idx of array) {
+    array0.push(appendString + idx);
+  }
+  return array0;
 }
