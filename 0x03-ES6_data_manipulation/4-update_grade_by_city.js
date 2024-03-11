cb09fe948a7b2d2186d85e8ad7645e5270588f49
ElRoccinante <alaealchi@gmail.com>
2024-03-11 23:24:18 +0000

    combine

---
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -0,0 +1,13 @@
+const updateStudentGradeByCity = (listOfStudents, city, newGrades) => (
+  listOfStudents.filter((el) => el.location === city)
+    .map((el) => {
+      const student = el;
+      student.grade = 'N/A';
+      return newGrades.map((item) => {
+        if (student.id === item.studentId) student.grade = item.grade;
+        return (student);
+      })[0];
+    })
+);
+
+export default updateStudentGradeByCity;
